849f406e9e3aefe0c659513bb226530793d4b77f
sadness52r <bukin.k2017@gmail.com>
2024-06-19 20:34:03 +0300

    add some logs of summ-func

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace Calculator
             Calculator calculator = new Calculator();
 
             Console.WriteLine($"3 + 2 = {calculator.Summ(3, 2)}");
+            Console.WriteLine($"-2 - 4 = {calculator.Summ(-2, -4)}");
+            Console.WriteLine($"0 + 0 = {calculator.Summ(0, 0)}");
+            Console.WriteLine($"-5 + 5 = {calculator.Summ(-5, 5)}");
         }
     }
 }
