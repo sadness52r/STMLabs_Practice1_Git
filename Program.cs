243606ae580232f663284b09887ad9e8bef24f41
sadness52r <bukin.k2017@gmail.com>
2024-06-19 21:14:06 +0300

    add some logs of div func

--- Program.cs
+++ Program.cs
@@ -28,6 +28,9 @@ namespace Calculator
 
             Console.WriteLine("Div function:");
             Console.WriteLine($"3 / 2 = {calculator.Div(3, 2)}");
+            Console.WriteLine($"-2 / 4 = {calculator.Div(-2, 4)}");
+            Console.WriteLine($"0 / 0 = {calculator.Div(0, 0)}");
+            Console.WriteLine($"-5 / 5 = {calculator.Div(-5, 5)}");
         }
     }
 }
